6d317da0ea651128e2ebc6fd7dac62b748a73564
AgustinTafura <agustintafura@hotmail.com>
2022-04-18 00:20:36 -0300

    add table

--- src/App.jsx
+++ src/App.jsx
@@ -4,176 +4,29 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import React, { useState, useContext } from 'react';
 import SideBar from './components/SideBar';
-import DateFilter from "./components/DateFilter";
-import { BotsProvider } from "./contexts/BotsContext";
+import { ClientsProvider } from "./contexts/ClientsContext";
+import CasesTable from "./components/CasesTable";
 
 
 function App() {
 
-  const [botActive, setBotActive] = useState()
-  const [startDate, setStartDate] = useState();
-  const [endDate, setEndDate] = useState();
 
   return (
     
-    <BotsProvider>
-      
-    
+    <ClientsProvider>     
       <div className="App container-fluid">
         <div className="row">
-          <SideBar setBotActive={setBotActive}/>
+          <SideBar/>
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 className="h2">REPORTES</h1>
-                <div className="btn-toolbar mb-2 mb-md-0">
-                  <div className="input-group rounded">
-                    <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-                    <span className="input-group-text border-0" id="search-addon">
-                      <i className="fas fa-search"></i>
-                    </span>
-                </div>
-              </div>
+            <div
+              className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              <h1 className="h2">REPORTES</h1>
             </div>
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-              <h4>Detalle dashboard</h4>
-                <div className="d-flex">
-                  <i className="fa fa-calendar fa-lg align-self-center me-2"></i>
-                  <DateFilter startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate}/>
-            </div>
-
-            </div>
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                  <tr>
-                    <td>1,002</td>
-                    <td>placeholder</td>
-                    <td>irrelevant</td>
-                    <td>visual</td>
-                    <td>layout</td>
-                  </tr>
-                  <tr>
-                    <td>1,003</td>
-                    <td>data</td>
-                    <td>rich</td>
-                    <td>dashboard</td>
-                    <td>tabular</td>
-                  </tr>
-                  <tr>
-                    <td>1,003</td>
-                    <td>information</td>
-                    <td>placeholder</td>
-                    <td>illustrative</td>
-                    <td>data</td>
-                  </tr>
-                  <tr>
-                    <td>1,004</td>
-                    <td>text</td>
-                    <td>random</td>
-                    <td>layout</td>
-                    <td>dashboard</td>
-                  </tr>
-                  <tr>
-                    <td>1,005</td>
-                    <td>dashboard</td>
-                    <td>irrelevant</td>
-                    <td>text</td>
-                    <td>placeholder</td>
-                  </tr>
-                  <tr>
-                    <td>1,006</td>
-                    <td>dashboard</td>
-                    <td>illustrative</td>
-                    <td>rich</td>
-                    <td>data</td>
-                  </tr>
-                  <tr>
-                    <td>1,007</td>
-                    <td>placeholder</td>
-                    <td>tabular</td>
-                    <td>information</td>
-                    <td>irrelevant</td>
-                  </tr>
-                  <tr>
-                    <td>1,008</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                  <tr>
-                    <td>1,009</td>
-                    <td>placeholder</td>
-                    <td>irrelevant</td>
-                    <td>visual</td>
-                    <td>layout</td>
-                  </tr>
-                  <tr>
-                    <td>1,010</td>
-                    <td>data</td>
-                    <td>rich</td>
-                    <td>dashboard</td>
-                    <td>tabular</td>
-                  </tr>
-                  <tr>
-                    <td>1,011</td>
-                    <td>information</td>
-                    <td>placeholder</td>
-                    <td>illustrative</td>
-                    <td>data</td>
-                  </tr>
-                  <tr>
-                    <td>1,012</td>
-                    <td>text</td>
-                    <td>placeholder</td>
-                    <td>layout</td>
-                    <td>dashboard</td>
-                  </tr>
-                  <tr>
-                    <td>1,013</td>
-                    <td>dashboard</td>
-                    <td>irrelevant</td>
-                    <td>text</td>
-                    <td>visual</td>
-                  </tr>
-                  <tr>
-                    <td>1,014</td>
-                    <td>dashboard</td>
-                    <td>illustrative</td>
-                    <td>rich</td>
-                    <td>data</td>
-                  </tr>
-                  <tr>
-                    <td>1,015</td>
-                    <td>random</td>
-                    <td>tabular</td>
-                    <td>information</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <CasesTable/>
           </main>
         </div>
       </div>
-    </BotsProvider>
+    </ClientsProvider>
   );
 }
 
---
+++ src/components/CasesTable/index.jsx
@@ -0,0 +1,107 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ClientsContext } from '../../contexts/ClientsContext'
+import DateFilter from "../DateFilter/index";
+
+
+const CasesTable = () => {
+
+    const {clientSelected, casesData, fetchCasesData} = useContext(ClientsContext)
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        console.log(333, startDate, endDate)    
+        if (startDate && endDate){
+            fetchCasesData(clientSelected, startDate, endDate, 1)
+            setCurrentPage(1)
+        }
+    }, [startDate, endDate])
+    
+
+
+    if (clientSelected === undefined) { return <div>Seleciona un Cliente de la lista</div>}
+
+    return (
+        <>
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+                <h4>Detalle dashboard</h4>
+                <div className="d-flex">
+                    <i className="fa fa-calendar fa-lg align-self-center me-2"></i>
+                    <DateFilter startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate} />
+                </div>
+            </div>
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="col">Gestionado</th>
+                            <th scope="col">ID Caso</th>
+                            <th scope="col">Telefono</th>
+                            <th scope="col">DNI</th>
+                            <th scope="col">Grupo</th>
+                            <th scope="col">Orden</th>
+                            <th scope="col">Llamada</th>
+                            <th scope="col">Estado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            casesData?.results?.map((event, index)=>{  
+                                return (
+                                    <tr key={Math.random()}>
+                                        <td>{event.last_updated}</td>
+                                        <td>{event.id}</td>
+                                        <td>{event.phone}</td>
+                                        <td>{event.extra_metadata.id}</td>
+                                        <td>{event.extra_metadata.group}</td>
+                                        <td>{event.extra_metadata.orden}</td>
+                                        <td>{event.case_duration}</td>
+                                        <td>{event.case_result.name}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+
+                    </tbody>
+                </table>
+
+                {
+                    casesData?.results.length > 0 && (
+
+                        <nav aria-label="Page navigation example">
+                            <ul className="pagination justify-content-center">
+                                {
+                                    casesData.previous && (
+                                        <li className="page-item">
+                                            <a className="page-link" href="/"
+                                                onClick={(e)=>{e.preventDefault(); fetchCasesData(clientSelected, startDate, endDate, Number(currentPage)-1); setCurrentPage(currentPage-1)}}
+                                            >
+                                                Previous
+                                            </a>
+                                        </li>
+                                    )
+
+                                }
+                                <li className="page-item"><a className="page-link" href="/" onClick={(e)=>{e.preventDefault();}}>{currentPage}</a></li>
+                                {
+                                    casesData.next && (
+                                        <li className="page-item">
+                                            <a className="page-link" href="/" 
+                                                onClick={(e)=>{e.preventDefault(); fetchCasesData(clientSelected, startDate, endDate, currentPage+1); setCurrentPage(currentPage+1); }}
+                                            >
+                                                    Next
+                                            </a>
+                                        </li>
+                                    )
+                                }
+                            </ul>
+                        </nav>
+                    )
+                }        
+            </div>
+        </>
+  )
+}
+
+export default CasesTable
